df2f4cd3595087fe403c65cf8090c207fdd2761f
Nitesh Singh <niteshkumar789096@gmail.com>
2025-08-03 12:46:41 +0530

    appliction connection with moongoose

--- src/app.js
+++ src/app.js
@@ -1,21 +1,7 @@
 const express = require("express");
+const connect=require("./config/database");
 const app = express(); // instance of server
-
-// request handler for the root path "/"
-app.get("/", (req, res) => {
-  res.send("hello from the server ...");
-});
-//only execute that code if the request is a GET request and the path matches exactly.
-
-// request handler for the "/test" path
-app.get("/test", (req, res) => {
-  res.send("hello from the test...");
-});
-
-// request handler for the "/home" path
-app.get("/home", (req, res) => {
-  res.send("hello from the home ...");
-});
+const connect();
 
 app.listen(100, () => {
   console.log("hello, i am listening");
